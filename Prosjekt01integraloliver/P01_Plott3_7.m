b2539e605183499373e4e22d6ac4a25b1d5a2406
Oliver <ol.salvesen@gmail.com>
2015-10-22 21:56:43 +0200

    Removed handles

--- Prosjekt01integraloliver/P01_Plott3_7.m
+++ Prosjekt01integraloliver/P01_Plott3_7.m
@@ -1,34 +1,31 @@
 
 
 
-% f1 = figure(1) ;     %Sets frame to 1
-% %Sets position for frame
-% set(f1,'Position',...
-%     [(scrsz(3)/3 - ((scrsz(3)/3)/2)) scrsz(4)/3 scrsz(3)/3 scrsz(4)/3]);
-% subplot(3,1,1)
-% plot(Tid(1:k),Lys(1:k),Tid(1:k),nullp(1:k))     %plots light signal and zero
-% title('Lyssignal RAW')                          
-% xlabel('tid [sekund]')
-% ylabel('Lyssignal')
-set(h1,'XData',Tid(1:k),'YData',Lys(1:k)) 
-set(h2,'XData',Tid(1:k),'YData',nullp(1:k))
+f1 = figure(1) ;     %Sets frame to 1
+%Sets position for frame
+set(f1,'Position',...
+    [(scrsz(3)/3 - ((scrsz(3)/3)/2)) scrsz(4)/3 scrsz(3)/3 scrsz(4)/3]);
+subplot(3,1,1)
+plot(Tid(1:k),Lys(1:k),Tid(1:k),nullp(1:k))     %plots light signal and zero
+title('Lyssignal RAW')                          
+xlabel('tid [sekund]')
+ylabel('Lyssignal')
 
 
-% subplot(3,1,2)
-% plot(Tid(1:k),avvik(1:k),Tid(1:k),nulll(1:k))   %plots deviation and zero(real)
-% title('Avvik fra nullpunkt og nåværende lyssignal')
-% xlabel('tid [sekund]')
-% ylabel('Avviket')
-set(h3,'XData',Tid(1:k),'YData',avvik(1:k)) 
-set(h4,'XData',Tid(1:k),'YData',nulll(1:k))
+
+subplot(3,1,2)
+plot(Tid(1:k),avvik(1:k),Tid(1:k),nulll(1:k))   %plots deviation and zero(real)
+title('Avvik fra nullpunkt og nåværende lyssignal')
+xlabel('tid [sekund]')
+ylabel('Avviket')
 
 
-% subplot(3,1,3)
-% plot(Tid(1:k),LysIntegrert(1:k),Tid(1:k),nulll(1:k))    %Plots integrated and zero(real)
-% title('Lys integrert rundt nullpunktet')
-% xlabel('tid [sekund]')
-% ylabel('Areal')
-set(h5,'XData',Tid(1:k),'YData',LysIntegrert(1:k)) 
-set(h6,'XData',Tid(1:k),'YData',nulll(1:k))
+
+subplot(3,1,3)
+plot(Tid(1:k),LysIntegrert(1:k),Tid(1:k),nulll(1:k))    %Plots integrated and zero(real)
+title('Lys integrert rundt nullpunktet')
+xlabel('tid [sekund]')
+ylabel('Areal')
+
 
 drawnow
