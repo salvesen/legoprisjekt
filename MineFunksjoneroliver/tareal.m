9d65cd98ebe4ea5f44982e6ed59f26725fb46719
Oliver <ol.salvesen@gmail.com>
2015-11-02 19:50:34 +0100

    new function

---
+++ MineFunksjoneroliver/tareal.m
@@ -0,0 +1,9 @@
+function [tangensareal ] = tareal(hosligende,vinkel )
+%TAREAL Summary of this function goes here
+%   Detailed explanation goes here
+    motstaaende = hosligende*tand(vinkel);
+    tangensareal = (motstaande*hosligende)/2;
+
+
+end
+
